213eac0b1200d8404e977060b29a0af8051967b7
Ye Joo Park <subwaymatch@gmail.com>
2018-10-04 08:20:44 -0700

    Create database if not exists

--- create-tables.sql
+++ create-tables.sql
@@ -1,4 +1,10 @@
 -- -----------------------------------------------------
+-- Database movie_review_app
+-- -----------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `movie_review_app` DEFAULT CHARACTER SET utf8 ;
+USE `movie_review_app` ;
+
+-- -----------------------------------------------------
 -- Table `users`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `users` ;
@@ -8,6 +14,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` VARCHAR(100) NULL,
   `date_of_birth` DATE NULL,
   PRIMARY KEY (`id`))
+COLLATE = 'utf8_general_ci'
 ENGINE = InnoDB;
 
 
@@ -22,6 +29,7 @@ CREATE TABLE IF NOT EXISTS `movies` (
   `genre` VARCHAR(100) NULL,
   `release_date` DATE NULL,
   PRIMARY KEY (`id`))
+COLLATE = 'utf8_general_ci'
 ENGINE = InnoDB;
 
 
@@ -36,6 +44,7 @@ CREATE TABLE IF NOT EXISTS `actors` (
   `gender` VARCHAR(100) NULL,
   `date_of_birth` DATE NULL,
   PRIMARY KEY (`id`))
+COLLATE = 'utf8_general_ci'
 ENGINE = InnoDB;
 
 
@@ -54,13 +63,14 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   CONSTRAINT `fk_reviews_users`
     FOREIGN KEY (`users_id`)
     REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_reviews_movies1`
     FOREIGN KEY (`movies_id`)
     REFERENCES `movies` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+COLLATE = 'utf8_general_ci'
 ENGINE = InnoDB;
 
 
@@ -77,11 +87,12 @@ CREATE TABLE IF NOT EXISTS `lead` (
   CONSTRAINT `fk_lead_movies1`
     FOREIGN KEY (`movies_id`)
     REFERENCES `movies` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_lead_actors1`
     FOREIGN KEY (`actors_id`)
     REFERENCES `actors` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+COLLATE = 'utf8_general_ci'
 ENGINE = InnoDB;
